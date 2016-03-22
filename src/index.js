a88d3244564ad23ff3d57b6fee2a5733cf963ea4
Zack Winchell <zackwinchell@gmail.com>
2016-03-22 03:20:08 -0700

    adds scss loader to webpack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 
 import Splash from './components/Splash';
 
+require("./style.scss");
 
 ReactDOM.render(
   <Splash />
--- webpack.config.js
+++ webpack.config.js
@@ -22,16 +22,13 @@ module.exports = {
     ],
     loaders: [
       {
-        test: /\.css$/,
-        loader: "style!css"
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "babel",
-        query: {
-          presets: ["react", "es2015"]
-        }
+        loaders: ["babel"]
       }
     ]
   },
