42d786841091a01f47ac3b3ff04b19fe772a6080
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-26 11:39:44 -0700

    styles leaguepanel

--- src/components/LeaguesPanel.js
+++ src/components/LeaguesPanel.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 export default class LeaguesPanel extends Component {
   render() {
     return (
-      <div>Hello from LeaguesPanel</div>
+      <div className="col-xs-3 league-panel">
+        <h3>My Leagues</h3>
+        <h6>Lorem ipsum dolor sit.</h6>
+        <p>Lorem ipsum</p>
+        <h6>Lorem ipsum dolor sit.</h6>
+        <p>Lorem ipsum</p>
+        <h6>Lorem ipsum dolor sit.</h6>
+        <p>Lorem ipsum</p>
+      </div>
     )
   }
 }
