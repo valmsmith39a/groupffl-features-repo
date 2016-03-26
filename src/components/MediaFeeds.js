a5ed3e0148cc62abb1864c7738826b85c71deb86
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-26 16:58:15 -0700

    includes #fantasyfootball twitter feed

--- src/components/MediaFeeds.js
+++ src/components/MediaFeeds.js
@@ -6,11 +6,13 @@ export default class MediaFeeds extends Component {
     const placehold2 = "http://placehold.it/300x400";
     return (
       <div className="col-xs-3 media-feeds">
+        {/*}
         <div className="row">
           <img className="img-responsive feeds" src={placehold1} />
         </div>
+        */}
         <div className="row">
-          <img className="img-responsive feeds" src={placehold2} />
+          <a className="twitter-timeline" href="https://twitter.com/hashtag/FantasyFootball" data-widget-id="713864014485282816">#FantasyFootball Tweets</a>
         </div>
       </div>
     );
