9da86d157f1cfa79d69f01c32d1046bbe084f409
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-26 15:25:05 -0700

    validates register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -22,29 +22,38 @@ class RegisterForm extends Component {
         <form
           onSubmit={handleSubmit(this.onSubmit.bind(this))}
           className="col-xs-10 col-xs-offset-1">
-          <div className="form-group">
+          <div className={`form-group ${email.touched && email.invalid ? 'has-danger' : ''}`}>
             <label>Email address</label>
             <input
               type="text"
               className="form-control"
-              placeholder="Email"
+              placeholder="Enter an email"
               {...email} />
+            <div className="text-help">
+              {email.touched ? email.error : ''}
+            </div>
           </div>
-          <div className="form-group">
+          <div className={`form-group ${password.touched && password.invalid ? 'has-danger' : ''}`}>
             <label>Password</label>
             <input
               type="password"
               className="form-control"
               placeholder="Password"
               {...password} />
+            <div className="text-help">
+              {password.touched ? password.error : ''}
+            </div>
           </div>
-          <div className="form-group">
+          <div className={`form-group ${password2.touched && password2.invalid ? 'has-danger' : ''}`}>
             <label>Password (again)</label>
             <input
               type="password"
               className="form-control"
               placeholder="Password"
               {...password2} />
+            <div className="text-help">
+              {password2.touched ? password2.error : ''}
+            </div>
           </div>
           <button type="submit" className="btn btn-default">Register</button>
         </form>
@@ -54,8 +63,27 @@ class RegisterForm extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = 'Enter an email';
+  }
+  if (!values.password) {
+    errors.password = 'Enter a password';
+  }
+  if (!values.password2) {
+    errors.password2 = 'Enter a password';
+  }
+
+  if (values.password !== values.password2) {
+    errors.password2 = 'Passwords Do Not Match'
+  }
+  return errors;
+}
+
 export default reduxForm({
   form: 'RegisterForm',
-  fields: ['email', 'password', 'password2']
-  // validate
+  fields: ['email', 'password', 'password2'],
+  validate
 }, null, { registerUser } )(RegisterForm);
