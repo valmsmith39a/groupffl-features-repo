27b39f0884f3d50d3ba8087fe6f112b96bcb3f35
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-26 11:54:49 -0700

    styles join league

--- src/components/JoinLeague.js
+++ src/components/JoinLeague.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export default class JoinLeague extends Component {
   render() {
     return (
-      <div>Join League</div>
+      <div className="col-xs-6 join-league-form">
+        <h2 className="login-title">Login</h2>
+        <form className="col-xs-10 col-xs-offset-1">
+          <div className="form-group">
+            <label>League Id</label>
+            <input type="email" className="form-control" placeholder="Enter League Id" />
+          </div>
+          <div className="form-group">
+            <label>Team Name</label>
+            <input type="password" className="form-control" placeholder="Enter Team Name" />
+          </div>
+          <button type="submit" className="btn btn-default">Login</button>
+        </form>
+      </div>
     );
   }
 }
