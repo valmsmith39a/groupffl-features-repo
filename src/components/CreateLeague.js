d77bfe713bc96309820176206c1fddd68ae2ef5d
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-26 11:57:49 -0700

    styles create league

--- src/components/CreateLeague.js
+++ src/components/CreateLeague.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 export default class CreateLeague extends Component {
   render() {
     return (
-      <div>Create League</div>
+      <div className="col-xs-6 create-league-form">
+        <h2 className="login-title">Create League</h2>
+        <form className="col-xs-10 col-xs-offset-1">
+          <div className="form-group">
+            <label>League Name</label>
+            <input type="email" className="form-control" placeholder="Enter League Name" />
+          </div>
+          <div className="form-group">
+            <label>Fantasy League Url</label>
+            <input type="email" className="form-control" placeholder="Enter The Url For Your Fantasy League" />
+          </div>
+          <div className="form-group">
+            <label>Team Name</label>
+            <input type="password" className="form-control" placeholder="Enter Team Name" />
+          </div>
+          <button type="submit" className="btn btn-default">Create</button>
+        </form>
+      </div>
     );
   }
 }
--- src/components/JoinLeague.js
+++ src/components/JoinLeague.js
@@ -4,7 +4,7 @@ export default class JoinLeague extends Component {
   render() {
     return (
       <div className="col-xs-6 join-league-form">
-        <h2 className="login-title">Login</h2>
+        <h2 className="login-title">Join League</h2>
         <form className="col-xs-10 col-xs-offset-1">
           <div className="form-group">
             <label>League Id</label>
@@ -14,7 +14,7 @@ export default class JoinLeague extends Component {
             <label>Team Name</label>
             <input type="password" className="form-control" placeholder="Enter Team Name" />
           </div>
-          <button type="submit" className="btn btn-default">Login</button>
+          <button type="submit" className="btn btn-default">Join</button>
         </form>
       </div>
     );
