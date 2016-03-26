a81993a18b2669cae69b5d0b6baf1d3cd10f17f8
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-25 23:23:43 -0700

    adds JoinPanel to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
 import Navbar from './Navbar';
+import JoinPanel from './JoinPanel';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <JoinPanel />
         {/*
         {this.props.children}
-        <JoinPanel />
         <MyLeaguesPanel />
       */}
       </div>
