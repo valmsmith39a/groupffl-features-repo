38684877295440d9bc23e26014e2a75eba63f05f
valmsmith39a <valmsmith39a@gmail.com>
2016-03-27 22:16:57 -0700

    Implements action creator to make GET request to obtain leagues.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,13 @@ export const REGISTER_USER = 'REGISTER_USER';
 export const LOGIN_USER = 'LOGIN_USER';
 export const CREATE_LEAGUE = 'CREATE_LEAGUE';
 export const JOIN_LEAGUE = 'JOIN_LEAGUE';
+export const FETCH_LEAGUES = 'FETCH_LEAGUES';
 
 const REGISTER_URL = '/api/user/register';
 const LOGIN_URL = '/api/user/login';
 const CREATE_LEAGUE_URL = '/api/league';
 const JOIN_LEAGUE_URL = '/api/team';
+const FETCH_LEAGUES_URL = '/api/league';
 
 export function registerUser(props) {
   const request = axios.post(REGISTER_URL, { email: props.email, password: props.password })
@@ -38,12 +40,19 @@ export function createLeague(props) {
 }
 
 export function joinLeague(props) {
-  console.log('props in join league action is: ', props);
-  
   const request = axios.post(JOIN_LEAGUE_URL, props);
 
   return {
     type: JOIN_LEAGUE,
     payload: request
   }
+}
+
+export function fetchLeagues(props) {
+    const request = axios.get(FETCH_LEAGUES_URL);
+
+    return {
+      type: FETCH_LEAGUES,
+      payload: request
+    }
 }
