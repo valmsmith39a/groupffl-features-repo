bda72bc90ee6c120b90ff5e2e608a6276cc6a5f0
valmsmith39a <valmsmith39a@gmail.com>
2016-03-27 23:29:52 -0700

    Implements id in params of url when navigating to league page

--- src/reducers/reducer_league.js
+++ src/reducers/reducer_league.js
@@ -1,18 +1,34 @@
+import { FETCH_LEAGUES } from '../actions/index';
+
+
+const INITIAL_STATE = { all: [], league: null };
+
+export default function(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case FETCH_LEAGUES:
+      return { all: action.payload.data };
+    default:
+      return state;
+  }
+}
+
+/*
 import { CREATE_LEAGUE } from '../actions/index';
 import { FETCH_LEAGUES } from '../actions/index';
 
-const INITIAL_STATE = { all: [], league: null}
+const INITIAL_STATE = { all: [], league: null }
 
 export default function (state = INITIAL_STATE, action) {
   console.log('action.payload', action.payload);
   switch(action.type) {
     case FETCH_LEAGUES:
       //return { ...state, post: action.payload.data}
-      return { all: action.payload.data };
+      return { ...state, all: action.payload.data };
     default:
       return state;
   }
 }
+*/
 
 //case CREATE_LEAGUE:
 //  return { league: action.payload };
