6e2ad78d22128c45f598ef5d24083bd616a21522
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-29 12:11:57 -0700

    styles media-panel with headers and glyphs

--- src/components/MediaVideo.js
+++ src/components/MediaVideo.js
@@ -6,14 +6,24 @@ export default class MediaVideo extends Component {
     return (
       <div className="col-xs-6 media-panel">
         <div>
-
           <h2>Connect with friends and the football world around you on GroupFFL.</h2>
-
-          <h4><Glyphicon glyph="comment" /> Talk fantasy football with friends</h4>
-          <h4>Get recent news from around the league</h4>
-          <h4>Find more of what you've been missing</h4>
+          <div className="icons-panel">
+            <span><Glyphicon glyph="comment" /></span>
+            <h4 className="bold">Talk fantasy football</h4>
+            <h4>with friends.</h4>
+          </div>
+          <div className="icons-panel">
+            <span><Glyphicon glyph="bullhorn" /></span>
+            <h4 className="bold">Get recent news</h4>
+            <h4>from around the league.</h4>
+          </div>
+          <div className="icons-panel">
+            <span><Glyphicon glyph="globe" /></span>
+            <h4 className="bold">Find more</h4>
+            <h4>of what you've been missing.</h4>
+          </div>
         </div>
-        <iframe width="100%" height="320px" src="https://www.youtube.com/embed/Y_NmwtttzA8" frameborder="0" allowfullscreen></iframe>
+        <iframe width="90%" height="320px" src="https://www.youtube.com/embed/Y_NmwtttzA8" frameborder="0" allowfullscreen></iframe>
       </div>
     );
   }
