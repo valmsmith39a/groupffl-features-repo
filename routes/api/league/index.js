8fc48a913a614791a1cc4f5df6c61ee70fa669e1
Zack Winchell <zackwinchell@gmail.com>
2016-03-29 20:54:39 -0700

    adds endpoint for league-specific data

--- routes/api/league/index.js
+++ routes/api/league/index.js
@@ -4,15 +4,33 @@
   const router = express.Router();
 
   const League = require(global.models + '/League');
+  const Post = require(global.models + '/Post');
+  const Team = require(global.models + '/Team');
   const User = require(global.models + '/User');
 
-  // router.get('/', (req, res) => {
-  //   League.find({}, (err, leagues) => {
-  //     if (err) { return res.status(400).send(err); }
-  //     console.log(leagues);
-  //     res.send(leagues);
-  //   }).populate('teams');
-  // });
+  router.get('/:leagueId', User.isLoggedIn, (req, res) => {
+    User.findById(req.user, (err, user) => {
+      if (err) { return res.status(400).send(err); }
+      if (user.leagues.indexOf(req.params.leagueId) === -1) { return res.status(400).send('You do not belong to this league'); }
+      League.findById(req.params.leagueId, (err, league) => {
+        if (err) { return res.status(400).send(err); }
+        if (!league) { return res.status(400).send('There is no league with this id'); }
+      })
+      .exec((err, league) => { // TODO: Change to promises and .then()
+        if (err) { return res.status(400).send(err); }
+        Team.populate(league, { path: 'teams' }, (err, league) => {
+          if (err) { return res.status(400).send(err); }
+          Post.populate(league, { path: 'posts teams.posts' }, (err, league) => {
+            if (err) { return res.status(400).send(err); }
+            User.populate(league, { path: 'posts.author teams.owner', model: 'User', select: 'email' }, (err, league) => {
+              if (err) { return res.status(400).send(err); }
+              res.send(league);
+            });
+          });
+        });
+      });
+    });
+  });
 
   router.get('/', User.isLoggedIn, User.getUserLeaguesMW, (req, res) => {
     console.log('userLeagues', req.userLeagues);
